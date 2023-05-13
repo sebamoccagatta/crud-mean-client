26291e1a466a52bb20bb5b7bf33e8414a222ae5b
seba_moccagatta <sebamoccagatta1@gmail.com>
2023-05-13 13:24:44 -0300

    create components, models and service

---
+++ src/app/models/producto.ts
@@ -0,0 +1,15 @@
+export class Producto {
+    _id?: number;
+    name: string;
+    category: string;
+    locate: string;
+    price: number;
+
+    constructor (name: string, category: string, locate: string, price: number){
+        this.name = name;
+        this.category = category;
+        this.locate = locate;
+        this.price = price;
+
+    }
+}

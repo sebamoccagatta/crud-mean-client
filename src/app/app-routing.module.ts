194c895281af0af11806895944c4f2de81498c3f
seba_moccagatta <sebamoccagatta1@gmail.com>
2023-05-13 13:30:33 -0300

    config routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ListarProductosComponent } from './components/listar-productos/listar-productos.component';
+import { CrearProductoComponent } from './components/crear-producto/crear-producto.component';
 
-const routes: Routes = [];
+const routes: Routes = [ 
+  {
+    path: '',
+    component: ListarProductosComponent
+  },
+  {
+    path: 'create-product',
+    component: CrearProductoComponent
+  },
+  {
+    path: 'edit-product/:id',
+    component: CrearProductoComponent
+  },
+  {
+    path: '**',
+    redirectTo: '',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
